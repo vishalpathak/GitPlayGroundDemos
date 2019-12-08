12d8094c8345962bbf680170f76126cd7a2efff7
vishalpathak <pathakvishal@gmail.com>
2019-12-08 22:01:16 +0530

    Added New code for Class with init example

--- AllNewDeoms.playground/Contents.swift
+++ AllNewDeoms.playground/Contents.swift
@@ -1,3 +1,17 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Simple Class with initializer
+class Person{
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
+
+let objPerson = Person(name: "Rock", age: 35)
+print("Person:\(objPerson.name, objPerson.age)")
